e3d5359395b4ab120313279e246fc4ea501fda46
chenjib <chenjibzh@163.com>
2020-01-13 10:18:31 +0800

    添加百度统计

---
+++ docs/.vuepress/public/js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?3a28a495de3e2613512d2d2ea8c880ae";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
