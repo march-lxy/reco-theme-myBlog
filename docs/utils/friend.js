81b81635b7a52e79e2bba3fcace54ad50fb397e0
ACchenjiangbo <916305619@qq.com>
2020-02-07 16:14:00 +0800

    更新友链存放目录

---
+++ docs/utils/friend.js
@@ -0,0 +1,45 @@
+var friendList = [{
+        name: "午后南杂",
+        desc: "Enjoy when you can, and endure when you must.",
+        link: "https://www.recoluan.com",
+        thumbnail: "https://www.recoluan.com/head.png"
+    },
+    {
+        name: "凉风有信",
+        desc: "责难无以成事",
+        link: "https://gleehub.com/",
+        thumbnail: "https://static.xmt.cn/cc50c217cbe342ce951324583f2c6139.png"
+    },
+    {
+        name: "知行合一",
+        desc: "一生伏首拜阳明",
+        link: "https://www.myx1997.top/",
+        thumbnail: "https://www.myx1997.top/nezha.png"
+    },
+    {
+        name: "LOGI",
+        desc: "会点代码的强迫症",
+        link: "https://logi.im",
+        thumbnail: "https://ae01.alicdn.com/kf/UTB8q2lYPFfFXKJk43Otxh4IPFXaQ.jpeg"
+    },
+    {
+        name: "阿业战记",
+        desc: "提升码农亩产，掰直码农方向",
+        link: "https://eeee.im",
+        thumbnail: "https://eeee.im/avatar.jpg"
+    },
+    {
+        name: "Quinn Tian's Blog",
+        desc: "一个专注于学习、记录和分享的个人网站",
+        link: "https://quinntian.com",
+        thumbnail: "https://quinntian.com/avatar"
+    },
+    {
+        name: "村头老杨头",
+        desc: "不忘初心、放得始终",
+        link: "http://ctlyt.yunypan.cn",
+        thumbnail: "http://ctlyt.yunypan.cn/avatar"
+    }
+]
+
+export default friendList
